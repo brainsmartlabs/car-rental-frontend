3b5edc767d04ff809210610290b9bd24f8621a41
Balaji The Boss <brainsmartlabs@gmail.com>
2022-11-01 16:18:38 +0530

    setting up redux store

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
 
 function Home() {
+  const { cars } = useSelector(state => state.carsReducer)
   return (
-    <div>Home</div>
+    <div>
+      <h1>Home</h1>
+      <h1>The length of cars array is {cars.length}</h1>
+    </div>
   )
 }
 
---
+++ src/redux/reducers/carsReducer.js
@@ -0,0 +1,9 @@
+const initialData = {
+    cars: []
+};
+
+export function carsReducer(state = initialData, action) {
+    switch (action.type) {
+        default: return state
+    }
+}
